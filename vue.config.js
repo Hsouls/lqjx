0de000c3ecaa3e083fc46b20850b1cacbae820dd
Hsouls <cp2327325672@126.com>
2025-04-27 17:49:42 +0800

    1

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' 
-    ? '/lqjx/'  // 例如：'/lqjx/'（根据你的仓库名 https://github.com/Hsouls/lqjx 填写）
-    : '/'  // 开发环境用根路径
-};
